e381bc945a2276802a3f601ba114c35370b1febe
prubayita <prubayita@gmail.com>
2023-07-27 09:18:54 +0200

    nn

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.district_report_view, name='district_report_view'),
     path('t', views.graph_data_view, name='graph_data_view'),
+    path('m', views.monthly, name='monthly'),
     path('table', views.table, name='table'),
     path('login', views.login, name='login')
 ]
