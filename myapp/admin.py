9372303adb88b0c0c00c3cbca93138c921d07531
prubayita <prubayita@gmail.com>
2023-08-22 10:45:21 +0200

    vv

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,7 +5,7 @@ from import_export.admin import ImportExportModelAdmin
 # from myapp.resources import ReportResource
 
 class ReportAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    list_display = ('bp_code', 'bp_name', 'district', 'profile', 'agreement_days', 'total_sales', 'contract_status','sales_month')
+    list_display = ('bp_code', 'bp_name', 'district', 'product', 'agreement_days', 'total_sales', 'contract_status','sales_month')
 
 admin.site.register(Report, ReportAdmin)
 
